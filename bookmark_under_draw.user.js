f3fab6377fdcfccde76d1256b4cb4ba1a2308a75
Jormund <etiennebotek@gmail.com>
2018-02-05 19:13:50 +0100

    cdn.rawgit url

--- bookmark_under_draw.user.js
+++ bookmark_under_draw.user.js
@@ -3,9 +3,10 @@
 // @name           IITC plugin: Bookmark portals under draw or search result.
 // @author         Jormund
 // @category       Controls
-// @version        0.1.7.20161122.1430
-// @description    [2016-11-22-1430] Bookmark portals under draw or search result.
-// @downloadURL    https://github.com/Jormund/bookmark_under_draw/raw/master/bookmark_under_draw.user.js
+// @version        0.1.7.20180205.1913
+// @description    [2018-02-05-1913] Bookmark portals under draw or search result.
+// @updateURL      https://cdn.rawgit.com/Jormund/bookmark_under_draw/master/bookmark_under_draw.meta.js
+// @downloadURL    https://cdn.rawgit.com/Jormund/bookmark_under_draw/master/bookmark_under_draw.user.js
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
