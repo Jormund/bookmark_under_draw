e6b5432f2f972ebb72f91744d49cc74f3c1b56ad
Jormund <etiennebotek@gmail.com>
2018-10-31 19:42:46 +0100

    activate on intel.ingress.com, changed download url to github

--- bookmark_under_draw.user.js
+++ bookmark_under_draw.user.js
@@ -3,19 +3,22 @@
 // @name           IITC plugin: Bookmark portals under draw or search result.
 // @author         Jormund
 // @category       Controls
-// @version        0.1.14.20180529.1446
-// @description    [2018-05-29-1446] Bookmark portals under draw or search result.
-// @updateURL      https://cdn.rawgit.com/Jormund/bookmark_under_draw/master/bookmark_under_draw.meta.js
-// @downloadURL    https://cdn.rawgit.com/Jormund/bookmark_under_draw/master/bookmark_under_draw.user.js
+// @version        0.1.15.20181031.1930
+// @description    [2018-10-31-1930] Bookmark portals under draw or search result.
+// @updateURL      https://github.com/Jormund/bookmark_under_draw/raw/master/bookmark_under_draw.meta.js
+// @downloadURL    https://github.com/Jormund/bookmark_under_draw/raw/master/bookmark_under_draw.user.js
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
 //Changelog
+//0.1.15: activate on intel.ingress.com, changed download url to github
 //0.1.14: remove bookmarks under draw or search
 //0.1.13: sort by name
 //0.1.12: buttons to clear draw and bookmarks
