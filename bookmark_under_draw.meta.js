b76de5f6fdeb6a8202fbd3e749e24af1391f538f
Jormund <etiennebotek@gmail.com>
2018-10-31 20:11:06 +0100

    raw.githubusercontent.com

--- bookmark_under_draw.meta.js
+++ bookmark_under_draw.meta.js
@@ -5,8 +5,8 @@
 // @category       Controls
 // @version        0.1.15.20181031.1930
 // @description    [2018-10-31-1930] Bookmark portals under draw or search result.
-// @updateURL      https://github.com/Jormund/bookmark_under_draw/raw/master/bookmark_under_draw.meta.js
-// @downloadURL    https://github.com/Jormund/bookmark_under_draw/raw/master/bookmark_under_draw.user.js
+// @updateURL      https://raw.githubusercontent.com/Jormund/bookmark_under_draw/master/bookmark_under_draw.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Jormund/bookmark_under_draw/master/bookmark_under_draw.user.js
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
